a574bb8276c559477ad8ff3915ba3e597fbe00ea
hunsoo0823 <hunsoo0823@naver.com>
2020-10-18 17:33:21 +0900

    recurision,iteration problem

---
+++ 4problem/antwarrior.py
@@ -0,0 +1,23 @@
+"""
+    - 입력조건
+        - 첫 줄에 식량창고의 개수 N이 주어진다.(3<=N<=100)
+        - 둘째 줄에 공백으로 구분되어 각 식량창고에 저장된 식량의 개수 K가 주어진다.( 0 <= K <= 1,000)
+    - 출력조건
+        - 첫째 줄에 개미 전사가 얻을 수 있는 식량의 최댓값을 출력하시오.
+"""
+# 정수 N을 입력 받기
+n = int(input())
+# 식량 정보 입력받기
+array = list(map(int, input().split(' ')))
+
+d = [0] * 100
+
+# 다이나믹 프로그래밍(Dynamic Programming) 진행
+d[0] = array[0]
+d[1] = max(array[0], array[1])
+
+for i in range(2,n):
+    d[i] = int(d[i-2]) + array[i]
+    d[i] = max(d[i-1], d[i])
+
+print(d[n-1])
---
+++ 4problem/ricecake.py
@@ -0,0 +1,35 @@
+"""
+ - 입력 조건
+    -  첫째 줄에 떡의 개수 N과 요청한 떡의 길이 M이 주어진다. ( 1<=N<=1,000,000, 1<=M<=2,000,000)
+    -  둘째 줄에는 떡의 개별 높이가 주어진다. 떡 높이의 총합은 항상 M 이상이므로, 손님은 필요한 양만큼 덕을 사갈수 있다.
+    높이는 10억보다 작거나 같은 양의 정수 또는 0이다.
+ - 출력 조건
+    - 적어도 M만큼의 떡을 집에 가져가기 위해 절단기에 설정할 수 있는 높이의 최댓값을 출력한다.
+"""
+
+# n과 md의 갯수 입력받기
+n, m = map(int, input().split(' '))
+
+# 떡의 개별 높이 입력받기
+array = list(map(int, input().split(' ')))
+
+start = 0
+end = max(array)
+
+result = 0
+while(start <= end):
+    total = 0
+    mid = (start + end) // 2
+    # 떡 자르기
+    for i in array:
+        if mid < i:
+            total += i - mid
+    # 떡의 길이가 부족할 경우
+    if total < m:
+        end = mid - 1
+    # 떡의 길이가 남을 경우
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
