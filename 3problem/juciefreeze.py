6608ccfbb54244cc7d46bfbf7dbfb73b747b1d0b
hunsoo0823 <hunsoo0823@naver.com>
2021-04-24 22:45:58 +0900

    ICE

--- 3problem/juciefreeze.py
+++ 3problem/juciefreeze.py
@@ -6,68 +6,33 @@
  한번에 만들 수 있는 아이스크림의 개수를 출력한다.
 """
 
-n,m = map(int, input().split())
-graph = []
+#n,m 입력받음
 
+n, m = map(int, input().split())
+
+graph = []
 for i in range(n):
-    graph.append(list(map(int,input())))
+    graph.append(list(map(int, input())))
 
 def dfs(x,y):
-    if x <= -1 or x >= n or y <= -1 or y >= m:
+    if x < 0 or x > n-1 or y < 0 or y > n-1:
         return False
 
     if graph[x][y] == 0:
         graph[x][y] = 1
-        dfs(x - 1, y)
-        dfs(x + 1, y)
-        dfs(x, y - 1)
-        dfs(x, y + 1)
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
         return True
     return False
 
 count = 0
-
 for i in range(n):
     for j in range(m):
         if dfs(i,j) == True:
             count += 1
 
 print(count)
-
-
-
-
-
-
-"""
-n, m = map(int, input().split())
-graph = []
-
-for i in range(n):
-    graph.append(list(map(int, input())))
-
-def dfs(x,y):
-    if x <= -1 or x >= n or y <= -1 or y >= m:
-        return False
-
-    if graph[x][y] == 0:
-        graph[x][y] = 1
-        dfs(x-1,y)
-        dfs(x+1,y)
-        dfs(x,y-1)
-        dfs(x,y+1)
-        return True
-    return False
-
-result = 0
-
-for i in range(n):
-    for j in range(m):
-        if dfs(i,j) == True:
-            result += 1
-
-print(result)
-"""
-
 
 
