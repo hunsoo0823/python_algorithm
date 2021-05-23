61165e84af6418e713fc9824f4bc0886569a5fc0
hunsoo0823 <hunsoo0823@naver.com>
2021-05-23 19:02:41 +0900

    review

--- 4problem/FutureCity.py
+++ 4problem/FutureCity.py
@@ -7,35 +7,39 @@
      - 첫째 줄에 방문 판매원 A가 K번 회사를 거쳐 X번 회사로 가는 최소 이동시간을 출력한다.
      - 만약 X번 회사에 도달 할 수 없다면 -1을 출력한다.
 """
-# 무한을 의미하는 값으로 10억 설정
-INF = int(1e9)
+
+INF = int(1e9) # 무한을 의미하는 값으로 10억 설정
 
 # 노드의 개수 및 간선의 개수 입력받기
 n, m = map(int, input().split())
-# 2차원 리스트(그래프 표현)를 만들고, 모든 값을 무한으로 초기화
-graph = [[INF] * (n+1) for _ in range(n+1)]
 
-# 자기 자신에게 자기 자신으로 가는 비용 0으로 초기화
+# 2차원 리스트(그래프 표현)만들고, INF로 초기화
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+# 자기 자신에서 자기 자신으로 가는 비용 0으로 초기화
 for i in range(1, n+1):
     graph[i][i] = 0
 
 # 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
 for _ in range(m):
-    # A와 B가 서로에게 가는 비용은 1이라고 설정
     a, b = map(int, input().split())
     graph[a][b] = 1
     graph[b][a] = 1
 
-# 거쳐 갈 노드 X와 최종 목적지 K를 입력받기
+# 거쳐 갈 노드 X와 최종 목적지 노드 k를 입력받기
 x, k = map(int, input().split())
 
 # 점화식에 따라 플로이드 워셜 알고리즘 수행
 for k in range(1, n+1):
     for a in range(1, n+1):
-        for b in range(1 ,n+1):
+        for b in range(1, n+1):
             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
-if graph[1][k] + graph[k][x] < 1e9:
-    print(graph[1][k] + graph[k][x])
-else:
+# 수행된 결과를 출력
+distance = graph[1][k] + graph[k][x]
+
+# 도달 할 수 없는 경우, -1를 출력
+if distance >= INF:
     print("-1")
+else:
+    print(distance)
--- 4problem/telegram.py
+++ 4problem/telegram.py
@@ -11,49 +11,52 @@
 import heapq
 import sys
 
-INF = int(1e9)
-# 노드의 개수, 간선의 개수, 시작 노드를 입력받기
-n, m, c = map(int, sys.stdin.readline().split())
-# 시작 지점
-start = c
-# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트
-graph = [[] for i in range(n+1)]
+input = sys.stdin.readline
+INF = int(1e9) # 무한을 의미하는 값으로 10억을 설
+
+# 노드의 개수, 간선의 개수, 시작 노드를 입력 받기
+n, m, start = map(int, input().split())
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트 만들기
+graph = [[] for _ in range(n+1)]
 # 최단 거리 테이블을 모두 무한으로 초기화
 distance = [INF] * (n+1)
 
-# 모든 간선 정보를 입력받긷
+# 모든 간선 정보를 입력받기
 for _ in range(m):
-    x, y, z = map(int,sys.stdin.readline().split())
+    x, y, z = map(int, input().split())
+    # x번 노드에서 y번 노드로 가는 비용 z라는 의미
     graph[x].append((y,z))
 
-def dijkstart(start):
+def dijkstra(start):
     q = []
-    # 시작 노드로 가기 위한 최단경로는 0으로 설정하며, 큐에 삽입
-    heapq.heappush(q , (0, start))
+    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하며, 큐에 삽입
+    heapq.heappush(q, (0, start))
     distance[start] = 0
-    while q: # 큐가 비어있지 않다면
-        # 가장 최단 거리가 짧은 노드에 대한 정보를 꺼내기
-        dist, now = heapq.heappop(q)
 
+    while q: # 큐가 비어있지 않다면
+        # 최단 거리가 짧은 노드에 대한 정보를 꺼내기
+        dist, now = heapq.heappop(q)
         if distance[now] < dist:
             continue
-        # 연결 되어 있는 다른 노드 확인
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
         for i in graph[now]:
             cost = dist + i[1]
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
 
-print(distance)
+# 다익스트라 알고리즘 수행
+dijkstra(start)
+
+# 도달 할 수 있는 노드의 개수
 count = 0
+# 도달 할 수 있는 노드 중에서, 가장 멀리 있는 노드와의 최단 거리
 max_distance = 0
-for i in distance:
-    if i != INF:
+
+for d in distance:
+    if d != INF:
         count += 1
-        max_distance = max(max_distance, i)
+        max_distance = max(max_distance, d)
 
-print(count - 1 , max_distance)
-
-
-
-
+# 시작 노드는 제외
+print(count -1, max_distance)
